85223c778e0525575667779b17af34433860eb31
GwonHeeJun <gwonhj9751@naver.com>
2020-04-09 14:37:31 +0900

    [UPDATE] 사용하는 폴더 및 파일 설정
    - 사용하는 폴더 생성 및 index.ts 파일 생성
    - 기존 js 파일을 ts & tsx 파일로 생성
    - webpack의 IterableIterator 타입 에러로 인해 tsconfig를 es6로 수정

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GlobalStyle } from './Style/GlobalStyle';
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div>☃️ 폰트 적용확인</div>
+    </React.Fragment>
+  )
+}
+
+export default App;
---
+++ src/Reducers/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+
+export const rootReducers = combineReducers({ });
+
+export type RootState = ReturnType<typeof rootReducers>;
