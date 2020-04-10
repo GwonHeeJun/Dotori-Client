c3e981a3e11ba4c02c0324e3fa880ea443cbe807
GwonHeeJun <gwonhj9751@naver.com>
2020-04-10 19:09:53 +0900

    [CREATE] 라우터 연결 및 에러 수정
    - notice, laptop 라우터 연결
    - 기존 webpack엔진에서 다른 라우터를 get하지 못하는 에러 수정
    ㄴ 404일 경우 index.html을 서빙하기 위해 historyApiFallback 옵션 추가
    - GlobalStyle의 Postioner 와 ContentWrapper 추가

--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,64 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: ['@babel/polyfill', `${__dirname}/src/index.tsx`],
-    output: {
-        path: `${__dirname}/dist`,
-        filename: '[name].[hash].js',
-        publicPath: '/',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: ['ts-loader'],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|svg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 100000,
-                        },
-                    },
-                ],
-            },
+  entry: ['@babel/polyfill', `${__dirname}/src/index.tsx`],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
         ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif|ttf|woff|woff2|eot|TTF)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+              publicPath: '/dist',
+              limit: 100000,
+            },
+          },
+        ],
+      },
     ],
-    devtool: 'inline-source-map',
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    inline: true,
+    port: 8080,
+    historyApiFallback: true,
+  },
 };
