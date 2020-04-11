c77ee59ad3f5362b93ec151a215bf9a0bdb4f3e4
GwonHeeJun <developer_gwon@naver.com>
2020-04-12 01:50:46 +0900

    [UPDATE] Sidebar Container, Component 제작.
    - 유저가 사용하기 위한 왼쪽 네비게이션 바 제작완료.
    - App에서 라우팅 되지 않으로 useState를 활용함.
    ㄴ useMemo를 활용하여 currentRoute가 변경될때마다 mapping을 새로함.
    - index.ts에서 Container와 Component를 통합하여 내보내줌.
    - 아직 반응형 레이아웃이 추가 안됨.
    ㄴ 다음커밋에 반응형 추가 예정임.

---
+++ src/Components/SiderbarItem/Style.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+interface StyleProps {
+    active: boolean
+}
+
+export const LinkWrapper = styled(Link)<StyleProps>`
+    text-decoration: none;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    color: ${props => props.active ? '#0F4C81' : '#BBBBBE'};
+    margin-bottom: 35px;
+
+    svg {
+        width: 25px;
+        height: 23.64px;
+        fill: ${props => props.active ? '#0F4C81' : '#BBBBBE'};
+    }
+`
+
+export const MenuTitle = styled.span`
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: 21px;
+`
---
+++ src/Container/SidebarContainer/SidebarContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Sidebar } from '../../Components';
+
+const SidebarContainer: React.FC = ( props ) => {
+    console.log(props);
+    return (
+        <Sidebar />
+    )
+}
+
+export default SidebarContainer;
