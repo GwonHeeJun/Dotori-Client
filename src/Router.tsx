70d9125dc1457acff9508fb3b24242776d5631a1
GwonHeeJun <developer_gwon@naver.com>
2020-04-12 23:47:55 +0900

    [UPDATE] 라우터 Component옵션 연결
    - 라우터의 component 옵션을 HomeContainer로 연결함

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { HomeContainer } from './Container';
 
 const Router: React.FC = () => {
   return (
     <React.Fragment>
-      <Route exact path="/" component={() => <div>메인 페이지</div>} />
+      <Route exact path="/" component={HomeContainer} />
       <Route path="/laptop" component={() => <div>노트북 대여 페이지</div>} />
       <Route path="/notice" component={() => <div>공지사항 페이지</div>} />
       <Route path="/notice/write" component={() => <div>공지사항 작성 페이지</div>} />
