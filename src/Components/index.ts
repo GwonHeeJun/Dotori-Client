ae004963b91ee0e52a701d4c00a0c27f5fdf8c1f
GwonHeeJun <gwonhj9751@naver.com>
2020-04-16 02:43:50 +0900

    [CREATE] 유저 프로필 컴포넌트 생성
    - 유저 정보 및 이벤트를 보기 위한 컴포넌트 생성
    - 내부 기능 최소화가 필요할 듯 함.

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -3,11 +3,13 @@ import SidebarList from './SidebarList/SidebarList';
 import SidebarItem from './SiderbarItem/SidebarItem';
 import BetaBanner from './Banners/BetaBanner/BetaBanner';
 import NotebookBanner from './Banners/NotebookBanner/NotebookBanner';
+import UserProfile from './UserProfile/UserProfile';
 
 export {
     Sidebar,
     SidebarList,
     SidebarItem,
     BetaBanner,
-    NotebookBanner
+    NotebookBanner,
+    UserProfile
 }; 
