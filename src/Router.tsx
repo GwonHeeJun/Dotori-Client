b002d389158e487ff0c9898e4652bfc4b918d0de
GwonHeeJun <gwonhj9751@naver.com>
2020-05-04 02:39:58 +0900

    [UPDATE] laptop 라우터 랜더 컨테이너 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { HomeContainer } from './Container';
+import { HomeContainer, LaptopContainer } from './Container';
 
 const Router: React.FC = () => {
   return (
     <React.Fragment>
       <Route exact path="/" component={HomeContainer} />
-      <Route path="/laptop" component={() => <div>노트북 대여 페이지</div>} />
+      <Route path="/laptop" component={LaptopContainer} />
       <Route path="/notice" component={() => <div>공지사항 페이지</div>} />
       <Route path="/notice/write" component={() => <div>공지사항 작성 페이지</div>} />
     </React.Fragment>
