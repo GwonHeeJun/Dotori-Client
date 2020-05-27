83c8ee65830f88ce2978aaee4631bb1f88769a0c
GwonHeeJun <gwonhj9751@naver.com>
2020-05-27 21:08:33 +0900

    modify : addtion event

--- src/Components/LaptopSeat/LaptopSeat.tsx
+++ src/Components/LaptopSeat/LaptopSeat.tsx
@@ -5,9 +5,13 @@ interface LaptopSeatProps {
     id : number;
 }
 
+const onClickRoomSeat = (id : number) => {
+    alert("노트북 대여 좌석 번호 : " + id);
+}
+
 const LaptopSeat : React.FC<LaptopSeatProps> = ({ id }) => {
     return (
-        <S.Postioner id={`${id + 1}`}>
+        <S.Postioner id={`${id + 1}`} onClick={() => onClickRoomSeat(id+1)}>
             <span>{id + 1}</span>
         </S.Postioner>
     )
